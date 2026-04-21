9bca9b2f4e6d82480bdec97cf5aff11f6f1c3a7d
Moblik-tech <kaike.dias.gabani@gmail.com>
2026-04-21 10:13:35 -0300

    Addition on run mechanic

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,8 +5,12 @@ public class Player : MonoBehaviour
     public Rigidbody2D rigidbody;
 
     public float speed = 5f;
-    public float forceJump = 3f;
+    public float speedRun = 25f;
+    private float _currentSpeed;
+
     public Vector2 friction = new(0.1f, 0);
+
+    public float forceJump = 3f;
 
     void Update()
     {
@@ -16,23 +20,26 @@ public class Player : MonoBehaviour
 
     void HandleMovement()
     {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            _currentSpeed = speedRun;
+        }
+        else
+        {
+            _currentSpeed = speed;
+        }
+
+        // Captação de input e aplicação de velocidade ao personagem
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            // opção 1
-            //rigidbody.MovePosition(rigidbody.position - velocity * Time.deltaTime);
-
-            // opção 2
-            rigidbody.velocity = new Vector2(-speed, rigidbody.velocity.y);
+            rigidbody.velocity = new Vector2(-_currentSpeed, rigidbody.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            // opção 1
-            //rigidbody.MovePosition(rigidbody.position + velocity * Time.deltaTime);
-
-            // opção 2
-            rigidbody.velocity = new Vector2(speed, rigidbody.velocity.y);
+            rigidbody.velocity = new Vector2(_currentSpeed, rigidbody.velocity.y);
         }
 
+        // Aplicação de fricção ao personagem
         if (rigidbody.velocity.x > 0)
         {
             rigidbody.velocity += friction;
