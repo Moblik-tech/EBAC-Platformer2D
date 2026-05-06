7634b4f4f1a941ad7967be5ae83c5857cafa88ce
Moblik-tech <kaike.dias.gabani@gmail.com>
2026-05-06 20:39:25 -0300

    Creation of Scriptable Object for player specific

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,34 +3,14 @@ using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
+    [Header("Setup")]
     public Rigidbody2D playerRigidbody;
-
-    [Header("Speed Setup")]
-    public float speed = 5f;
-    public float speedRun = 25f;
-    private float _currentSpeed;
-
-    public Vector2 friction = new(0.1f, 0);
-
-    public float forceJump = 3f;
-
-    [Header("Animation Setup")]
-    /*public float jumpScaleX = 0.7f;
-    public float jumpScaleY = 1.5f;
-    public float animationDuration = 0.3f;*/
-    public SOFloat soJumpScaleY;
-    public SOFloat soJumpScaleX;
-    public SOFloat soAnimationDuration;
-
-    public Ease ease = Ease.OutBack;
-
-    [Header("Animation player")]
-    public float playerSwipeDuration = 0.1f;
-    public string boolRun = "Run";
-    public string triggerDeath = "Death";
     public Animator animator;
-
     public HealthBase healthBase;
+
+    public SOPlayerSetup sOPlayerSetup;
+
+    private float _currentSpeed;
 
     private void Awake()
     {
@@ -44,7 +24,7 @@ public class Player : MonoBehaviour
     {
         healthBase.OnKill -= OnPlayerKill;
 
-        animator.SetTrigger(triggerDeath);
+        animator.SetTrigger(sOPlayerSetup.triggerDeath);
     }
 
     void Update()
@@ -57,49 +37,49 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            _currentSpeed = speedRun;
+            _currentSpeed = sOPlayerSetup.speedRun;
             animator.speed = 2;
         }
         else
         {
-            _currentSpeed = speed;
+            _currentSpeed = sOPlayerSetup.speed;
             animator.speed = 1;
         }
 
         // Captação de input e aplicação de velocidade ao personagem
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            animator.SetBool(boolRun, true);
+            animator.SetBool(sOPlayerSetup.boolRun, true);
             playerRigidbody.velocity = new Vector2(-_currentSpeed, playerRigidbody.velocity.y);
 
             if (playerRigidbody.transform.localScale.x != -1)
             {
-                playerRigidbody.transform.DOScaleX(-1, playerSwipeDuration);
+                playerRigidbody.transform.DOScaleX(-1, sOPlayerSetup.playerSwipeDuration);
             }
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            animator.SetBool(boolRun, true);
+            animator.SetBool(sOPlayerSetup.boolRun, true);
             playerRigidbody.velocity = new Vector2(_currentSpeed, playerRigidbody.velocity.y);
 
             if (playerRigidbody.transform.localScale.x != 1)
             {
-                playerRigidbody.transform.DOScaleX(1, playerSwipeDuration);
+                playerRigidbody.transform.DOScaleX(1, sOPlayerSetup.playerSwipeDuration);
             }
         }
         else
         {
-            animator.SetBool(boolRun, false);
+            animator.SetBool(sOPlayerSetup.boolRun, false);
         }
 
         // Aplicação de fricção ao personagem
         if (playerRigidbody.velocity.x > 0)
         {
-            playerRigidbody.velocity += friction;
+            playerRigidbody.velocity += sOPlayerSetup.friction;
         }
         else if (playerRigidbody.velocity.x < 0)
         {
-            playerRigidbody.velocity -= friction;
+            playerRigidbody.velocity -= sOPlayerSetup.friction;
         }
     }
 
@@ -107,7 +87,7 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            playerRigidbody.velocity = Vector2.up * forceJump;
+            playerRigidbody.velocity = Vector2.up * sOPlayerSetup.forceJump;
             playerRigidbody.transform.localScale = Vector2.one;
 
             DOTween.Kill(playerRigidbody.transform);
@@ -118,8 +98,8 @@ public class Player : MonoBehaviour
 
     void HandleScaleJump()
     {
-        playerRigidbody.transform.DOScaleY(soJumpScaleY.value, soAnimationDuration.value).SetLoops(2, LoopType.Yoyo).SetEase(ease);
-        playerRigidbody.transform.DOScaleX(soJumpScaleX.value, soAnimationDuration.value).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        playerRigidbody.transform.DOScaleY(sOPlayerSetup.jumpScaleY, sOPlayerSetup.animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(sOPlayerSetup.ease);
+        playerRigidbody.transform.DOScaleX(sOPlayerSetup.jumpScaleX, sOPlayerSetup.animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(sOPlayerSetup.ease);
     }
 
     public void DestroyMe()
---
+++ Assets/Scripts/Player/SOPlayerSetup.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using DG.Tweening;
+
+[CreateAssetMenu]
+public class SOPlayerSetup : ScriptableObject
+{
+    [Header("Speed Setup")]
+    public float speed = 5f;
+    public float speedRun = 25f;
+
+    public Vector2 friction = new(0.1f, 0);
+
+    public float forceJump = 3f;
+
+    [Header("Animation Setup")]
+    public float jumpScaleX = 0.7f;
+    public float jumpScaleY = 1.5f;
+    public float animationDuration = 0.3f;
+
+    public Ease ease = Ease.OutBack;
+
+    [Header("Animation player")]
+    public float playerSwipeDuration = 0.1f;
+    public string boolRun = "Run";
+    public string triggerDeath = "Death";
+}
