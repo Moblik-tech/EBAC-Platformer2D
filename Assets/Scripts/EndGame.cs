ae04f68097dd76cb73b0090a2ab75dc181c7875c
Moblik-tech <kaike.dias.gabani@gmail.com>
2026-06-05 19:31:27 -0300

    Setup for end game menu

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public string tagToCompare = "Player";
+
+    public GameObject uiEndGame;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag(tagToCompare))
+        {
+            CallEndGame();
+        }
+    }
+
+    public void CallEndGame()
+    {
+        uiEndGame.SetActive(true);
+    }
+}
